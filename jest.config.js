fd12e9e8f920f9fba7ed34bcd8bc33e897a7f8f8
Carlos Tremonte de Lemos <carlos.lemos_actdigital@prestador.globo>
2026-07-12 13:53:03 -0300

    feat(profileStats): verifica implementação completa do módulo de agregação estatística
    - Função profileStats(bubbles) exportada e testável
    - 10 testes cobrindo todos os casos obrigatórios (vazio, deduplicação, top 4 regiões, ordem determinística)
    - Deduplicação por creator funciona corretamente
    - formationTop usa nome de localidade, não continente
    - birthByRegion e formationByRegion usam campo continent
    - Build Vite passa sem erros

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 export default {
   testEnvironment: 'node',
   testMatch: ['**/src/lib/__tests__/**/*.test.js'],
+  transform: {},
+  extensionsToTreatAsEsm: ['.js'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
 };
